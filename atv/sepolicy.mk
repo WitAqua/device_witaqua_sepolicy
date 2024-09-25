eb2a00b4da3cd88e2737c520a849f00cdb5b1412
Toufu <hamm157famm7423@gmail.com>
2024-09-26 04:04:42 +0900

    sepolicy: Rebrand to WitAqua

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from WitAqua
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/witaqua/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/witaqua/sepolicy/atv/private
